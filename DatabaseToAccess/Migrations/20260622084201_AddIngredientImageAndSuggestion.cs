00b77ede057b2d66606b708c768a38a36a4055cc
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2026-06-22 11:43:30 +0300

    feat(data): add ingredient image and suggestion fields

---
+++ DatabaseToAccess/Migrations/20260622084201_AddIngredientImageAndSuggestion.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseToAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddIngredientImageAndSuggestion : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Image",
+                table: "Teas",
+                newName: "ImageUrl");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ImageUrl",
+                table: "Ingredients",
+                type: "text",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<Guid>(
+                name: "SuggestedUserId",
+                table: "Ingredients",
+                type: "uuid",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "ImageUrl",
+                table: "Ingredients");
+
+            migrationBuilder.DropColumn(
+                name: "SuggestedUserId",
+                table: "Ingredients");
+
+            migrationBuilder.RenameColumn(
+                name: "ImageUrl",
+                table: "Teas",
+                newName: "Image");
+        }
+    }
+}
