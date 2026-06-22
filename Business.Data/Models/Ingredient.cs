cc6399b0d69468c30335a85e8d3ef241e0af0b8e
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2026-06-22 11:39:28 +0300

    feat(Entities): add ingredient image

--- Business.Data/Models/Ingredient.cs
+++ Business.Data/Models/Ingredient.cs
@@ -8,5 +8,7 @@ namespace Business.Data.Models
         public string Name {get;set;} = string.Empty;
         public bool IsDeleted {get;set;} = false;
         public bool IsModerated {get;set;} = false;
+        public string ImageUrl {get;set;} = string.Empty;
+        public Guid? SuggestedUserId {get;set;}
     }
 }
