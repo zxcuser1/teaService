2ffa052596650284d74e3ffb46d0445686e18223
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2026-06-30 19:28:37 +0300

    feat(data): added migration on new user fields

---
+++ DatabaseToAccess/Migrations/20260630162624_AddUserSaltField.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace DatabaseToAccess.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddUserSaltField : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "Password",
+                table: "Users",
+                newName: "PasswordHash");
+
+            migrationBuilder.AddColumn<byte[]>(
+                name: "PasswordSalt",
+                table: "Users",
+                type: "bytea",
+                nullable: false,
+                defaultValue: new byte[0]);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "PasswordSalt",
+                table: "Users");
+
+            migrationBuilder.RenameColumn(
+                name: "PasswordHash",
+                table: "Users",
+                newName: "Password");
+        }
+    }
+}
