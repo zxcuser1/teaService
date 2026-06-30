fe269a39a94a02174ce731f2c411cb423308853c
zxcuser1 <126661647+zxcuser1@users.noreply.github.com>
2026-06-30 17:22:41 +0300

    feat(auth): add refresh token generation

--- Service.Application/TokenService/TokenService.cs
+++ Service.Application/TokenService/TokenService.cs
@@ -11,10 +11,21 @@ namespace Service.Application.TokenService
     public class TokenHelper
     {
         private readonly JwtParams _accessTokenParams;
-        
-        public TokenHelper(JwtParams accessTokenParams)
+        private readonly IUserSessionRepository _sessionRepo;
+        private readonly IRepository<RefreshToken> _tokenRepo;
+        private readonly IUnitOfWork _unitOfWork;
+        public TokenHelper
+        (
+            JwtParams accessTokenParams,
+            IUserSessionRepository sessionRepo,
+            IRepository<RefreshToken> tokenRepo,
+            IUnitOfWork unitOfWork
+        )
         {
             _accessTokenParams = accessTokenParams;
+            _sessionRepo = sessionRepo;
+            _tokenRepo = tokenRepo;
+            _unitOfWork = unitOfWork;
         }
         public string GenerateAccessToken(User user)
         {
@@ -37,6 +48,65 @@ namespace Service.Application.TokenService
 
             var encodedToken = new JwtSecurityTokenHandler().WriteToken(accessToken);
             return encodedToken;
+        }
+
+        public async Task<string> GenerateRefreshTokenAsync(Guid userId, string deviceId, CancellationToken token)
+        {
+            try {
+                await _unitOfWork.BeginTransactionAsync(token);
+
+                var session = await _sessionRepo.GetUserSessionAsync(userId, deviceId, token);
+
+                if (session == null)
+                {
+                    session = new UserSession
+                    {
+                        DeviceId = deviceId,
+                        UserId = userId
+                    };
+                    await _sessionRepo.AddAsync(session);
+                    await _unitOfWork.SaveChangesAsync(token);            
+                }
+
+                var now = DateTime.UtcNow;
+
+                var rawToken = Convert.ToHexString(
+                    RandomNumberGenerator.GetBytes(32));
+                
+                var tokenHash = CreateSHA512(rawToken);
+
+                var newToken = new RefreshToken
+                {
+                    TokenHash = tokenHash,
+                    ExpiresAt = now.AddDays(30),
+                    UserSessionId = session.Guid,
+                };
+
+                if (session.CurrentRefreshTokenId != null)
+                {
+                    var currentToken = await _tokenRepo.GetByIdAsync((Guid)session.CurrentRefreshTokenId, token) 
+                        ?? throw new Exception($"Found session {session.Guid} with invalid token id {session.CurrentRefreshTokenId}"); 
+                        //TODO make custom exception and specific logic to invalidate session!!
+
+                    currentToken.IsRevoked = true;
+                    currentToken.RevokedAt = now;
+                    newToken.PreviousTokenId = currentToken.Guid;
+                }
+
+                session.CurrentRefreshTokenId = newToken.Guid;
+
+                await _tokenRepo.AddAsync(newToken, token);
+
+                await _unitOfWork.SaveChangesAsync(token);
+
+                await _unitOfWork.CommitAsync(token);
+                return rawToken;
+            }
+            catch (Exception)
+            {
+                await _unitOfWork.RollbackAsync(CancellationToken.None);
+                throw;
+            }
         }
 
         public static string CreateSHA512(string input)
